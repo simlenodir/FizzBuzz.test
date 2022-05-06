b43fb6a29a8f896249f44807032385cd9b626a35
simlenodir <nodirsmailov0@gmail.com>
2022-05-06 17:50:23 +0300

    improved

--- js/index.js
+++ js/index.js
@@ -2,31 +2,42 @@ var fizz = 0
 var buzz = 0
 var fizzbuzz = 0 
 
+var elFizzlist = document.querySelector('.fizz-list')
+var elBuzzlist = document.querySelector('.buzz-list')
+var elFizzBuzzlist = document.querySelector('.fizzbuzz-list')
+
 var userNumber = prompt (`istalgan raqamingizni tanlang`) -0 ;
 
 document.getElementById("userNumber").innerHTML = userNumber ;
 
+
 if (userNumber < 0 ) {
-    document.getElementsByClassName.container.style.display = "none";
+    document.getElementsByClassName('').container.style.display = "none";
 } else {
-   document.getElementById("error").style.display = "none" ;
+    document.getElementById("error").style.display = "none" ;
 }
 
 for (i = 1; i <= userNumber;  i++ ) {
+    var elLi = document.createElement('li')
+    elLi.textContent = i
     if (i % 3 == 0 && i % 5== 0) {
-        fizzbuzz += 1 }
-        if (i % 5 === 0) {
+        fizzbuzz += 1 
+        elFizzBuzzlist.append(elLi)
+       }
+        
+     else  if (i % 5 === 0) {
+           elBuzzlist.append(elLi)
             buzz += 1
         }
 
-        if (i % 3 === 0) {
+        else if (i % 3 === 0) {
             fizz = fizz + 1
-           
+          elFizzlist.append(elLi) 
         }
     }
 
 
-document.getElementById("Fizz").innerHTML = fizz;
+document.getElementById("Fizz").innerHTML =  fizz;
 
 document.getElementById("Buzz").innerHTML = buzz;
 
